0949da80b4a59ee12d688e763466aa0270a39fd8
Солодянкина Ксения <k.solodyankina@orglot.office>
2019-12-05 16:23:15 +0300

    add assert to program

--- conftest.py
+++ conftest.py
@@ -1,20 +1,28 @@
 import pytest
 from selenium import webdriver
+from selenium.webdriver.chrome.options import Options
 
 def pytest_addoption(parser):
     parser.addoption('--browser_name', action='store', default="chrome",
                      help="Choose browser: chrome or firefox")
+    parser.addoption('--language', action='store', default=None,
+                     help="Choose language: ru, es, en, ...")
 
 @pytest.fixture(scope="function")
 def browser(request):
     browser_name = request.config.getoption("browser_name")
-    browser = None
+    user_language = request.config.getoption("language")
+    browser = None   
     if browser_name == "chrome":
-        browser = webdriver.Chrome()
-        browser.implicitly_wait(7)
+        options = Options()
+        options.add_experimental_option('prefs', {'intl.accept_languages':user_language})
+        browser = webdriver.Chrome(options=options)
+        browser.implicitly_wait(5)
     elif browser_name == "firefox":
-        browser = webdriver.Firefox()
-        browser.implicitly_wait(7)
+        fp = webdriver.FirefoxProfile()
+        fp.set_preference("intl.accept_lanuages", user_languae)
+        browser = webdriver.Firefox(firefox_profile=fp)
+        browser.implicitly_wait(5)
     else:
         raise pytest.UsageError("--browser_name should be chrome or firefox")
     yield browser
--- test_items.py
+++ test_items.py
@@ -1,9 +1,8 @@
-link = "http://selenium1py.pythonanywhere.com/"
+import pytest
+from selenium import webdriver
 
-def test_guest_should_see_login_link_pass(browser):
+def test_button_add_to_card(browser):
+    link = "http://selenium1py.pythonanywhere.com/catalogue/coders-at-work_207/"
     browser.get(link)
-    browser.find_element_by_css_selector("#login_link")
-
-def test_guest_should_see_login_link_fail(browser):
-    browser.get(link)
-    browser.find_element_by_css_selector("#magic_link")
+    button = len(browser.find_elements_by_css_selector(".btn.btn-add-to-basket"))
+    assert button > 0, "Button not found!"
